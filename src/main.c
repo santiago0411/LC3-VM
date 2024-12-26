d20547e7f1698476fac588888c959754b96a9b3b
santiago0411 <49250130+santiago0411@users.noreply.github.com>
2024-12-26 20:44:55 -0300

    Fix usage

--- src/main.c
+++ src/main.c
@@ -501,7 +501,7 @@ int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        printf("my-vm [image-file1] ...\n");
+        printf("%s [image-file1] ...\n", argv[0]);
         exit(2);
     }
 
